d7aec1fa9df7bae54fc44672e338c00188fdeef5
vp <mini32@gmail.com>
2021-03-05 14:14:01 +0100

    cc

--- Presentation.Web/Presentation.Web/Server/Controllers/DemoHttpWorkflowController.cs
+++ Presentation.Web/Presentation.Web/Server/Controllers/DemoHttpWorkflowController.cs
@@ -2,9 +2,6 @@
 using System.Threading;
 using System.Threading.Tasks;
 using Elsa.Models;
-using Elsa.Persistence;
-using Elsa.Persistence.Specifications;
-using Elsa.Persistence.Specifications.WorkflowInstances;
 using Elsa.Services;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -16,14 +13,11 @@ namespace Presentation.Web.Server.Controllers
     public class DemoHttpWorkflowController : Controller
     {
         private readonly IWorkflowRunner workflowRunner;
-        private readonly IWorkflowInstanceStore instanceStore;
 
         public DemoHttpWorkflowController(
-            IWorkflowRunner workflowRunner, 
-            IWorkflowInstanceStore instanceStore)
+            IWorkflowRunner workflowRunner)
         {
             this.workflowRunner = workflowRunner;
-            this.instanceStore = instanceStore;
         }
 
         [HttpPost]
@@ -40,11 +34,6 @@ namespace Presentation.Web.Server.Controllers
                 input: model,
                 correlationId: Guid.NewGuid().ToString("N"),
                 cancellationToken: cancellationToken);
-
-            //var results = await this.instanceStore.FindManyAsync(
-            //    new WorkflowDefinitionIdSpecification(nameof(DemoHttpWorkflow)),
-            //    OrderBySpecification.OrderByDescending<WorkflowInstance>(x => x.CreatedAt),
-            //    Paging.Page(1, 2));
 
             this.Response.StatusCode = StatusCodes.Status202Accepted;
             this.Response.Headers.Add("CorrelationId", instance.CorrelationId);
